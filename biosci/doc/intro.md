dec0537413d346438a69ce40e81851aa1e6e57dc
Marshall Thompson <marshall@cognitect.com>
2014-12-30 08:51:28 -0500

    Test

---
+++ biosci/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to biosci
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ biosci/test/biosci/core_test.clj
@@ -0,0 +1,7 @@
+(ns biosci.core-test
+  (:require [clojure.test :refer :all]
+            [biosci.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
